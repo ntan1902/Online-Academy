b942e973264519ceb80046296693cd36f09d5732
KhuongNguyen611 <khuong11b13@gmail.com>
2020-12-14 13:54:48 +0700

    login front-end

--- middlewares/controllers.mdw.js
+++ middlewares/controllers.mdw.js
@@ -3,9 +3,14 @@ module.exports = function (app) {
         res.render('home', {layout: 'main.hbs'})
     })
 
+    app.get('/login', function(req, res) {
+        res.render('vwLogin/login', {layout: 'main.hbs'})
+    } )
+
     app.use('/admin/students/', require('../controllers/student.controller'));
     app.use('/admin/courses/', require('../controllers/course.controller'));
     app.use('/admin/teachers/', require('../controllers/teacher.controller'));
+
 
     app.get('/err', function (req, res) {
         throw new Error('Error!');
