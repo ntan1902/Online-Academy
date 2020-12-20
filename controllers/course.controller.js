62d860eb8350cfebbc98e4cb2aed968042567074
ohnguyen2404 <50840294+ohnguyen2404@users.noreply.github.com>
2020-12-20 08:11:19 +0700

    Fix and finish 'prev' , 'next' button of pagnition
    Co-Authored-By: ntan1902 <50836735+ntan1902@users.noreply.github.com>

--- controllers/course.controller.js
+++ controllers/course.controller.js
@@ -1,64 +1,100 @@
-const express = require('express');
-const courseModel = require('../models/course.model');
-const { paginate } = require('../config/default.json');
+const express = require("express");
+const courseModel = require("../models/course.model");
+const { paginate } = require("../config/default.json");
 const moment = require("moment");
 
 const router = express.Router();
 
-router.get('/', async function(req, res, next) {
+router.get("/", async function (req, res, next) {
+  var page = req.query.page || 1;
+  if (page < 1) page = 1;
 
-    const page = req.query.page || 1;
-    if(page < 1) page = 1;
+  const total = await courseModel.countCourse();
+  let nPages = Math.floor(total / paginate.limit);
+  if (total % paginate.limit > 0) nPages++; //for the remaining courses
+  console.log(nPages);
+  const page_numbers = [];
 
-    const total = await courseModel.countCourse();
-    let nPages = Math.floor(total / paginate.limit);
-    if(total % paginate.limit > 0) nPages++; //for the remaining courses
-    console.log(nPages);
-    const page_numbers = [];
-    for(i=1; i <= nPages; i++) {
-        page_numbers.push({
-            value: i,
-            isCurrentPage: i === +page
-        });
+  let disablePrev = false;
+  let disableNext = false;
+  let prevPage, nextPage;
+  for (i = 1; i <= nPages; i++) {
+    let currentPage = (i === +page);
+    if (currentPage) {
+      if (i === 1) {
+        disablePrev = true;  
+      } else if (i === nPages) {
+        disableNext = true; 
+      }
+      prevPage = i - 1;
+      nextPage = i + 1;
     }
-  
-    const offset = (page - 1) * paginate.limit;
-    const list_courses = await courseModel.pageCourse(offset);
-
-    res.render('vwCourses/index', { 
-        courses: list_courses,
-        page_numbers,
-        empty: list_courses.length === 0
+    page_numbers.push({
+      value: i,
+      isCurrentPage: currentPage,
     });
+  }
+
+  const offset = (page - 1) * paginate.limit;
+  const list_courses = await courseModel.pageCourse(offset);
+
+  res.render("vwCourses/index", {
+    courses: list_courses,
+    page_numbers,
+    empty: list_courses.length === 0,
+    prevPage,
+    nextPage,
+    disablePrev,
+    disableNext
+  });
 });
 
-router.get('/byField/:field', async function (req, res) {
-    const field = req.params.field;
-    console.log(field);
+router.get("/byField/:field", async function (req, res) {
+  const field = req.params.field;
+  console.log(field);
 
-    const page = req.query.page || 1;
-    if(page < 1) page = 1;
+  var page = req.query.page || 1;
+  if (page < 1) page = 1;
 
-    const total = await courseModel.countCourseByField(field);
-    let nPages = Math.floor(total / paginate.limit);
-    if(total % paginate.limit > 0) nPages++; //for the remaining courses
-    console.log(nPages);
-    const page_numbers = [];
-    for(i=1; i <= nPages; i++) {
-        page_numbers.push({
-            value: i,
-            isCurrentPage: i === +page
-        });
+  const total = await courseModel.countCourseByField(field);
+  let nPages = Math.floor(total / paginate.limit);
+  if (total % paginate.limit > 0) nPages++; //for the remaining courses
+  console.log(nPages);
+  const page_numbers = [];
+
+  let disablePrev = false;
+  let disableNext = false;
+  let prevPage, nextPage;
+  for (i = 1; i <= nPages; i++) {
+    let currentPage = (i === +page);
+    if(currentPage) {
+      if(i === 1) {
+        disablePrev = true;
+      }
+      else if(i === nPages) {
+        disableNext = true;
+      }
+      prevPage = i - 1;
+      nextPage = i + 1;
     }
-  
-    const offset = (page - 1) * paginate.limit;
-    const list_courses = await courseModel.pageCourseByField(offset, field);
-
-    res.render('vwCourses/index', { 
-        courses: list_courses,
-        page_numbers,
-        empty: list_courses.length === 0
+    page_numbers.push({
+      value: i,
+      isCurrentPage: currentPage
     });
+  }
+
+  const offset = (page - 1) * paginate.limit;
+  const list_courses = await courseModel.pageCourseByField(offset, field);
+
+  res.render("vwCourses/index", {
+    courses: list_courses,
+    page_numbers,
+    empty: list_courses.length === 0,
+    prevPage,
+    nextPage,
+    disablePrev,
+    disableNext
+  });
 });
 // `id` int NOT NULL AUTO_INCREMENT,
 //   `imagePath` varchar(1000) DEFAULT NULL,
@@ -72,65 +108,67 @@ router.get('/byField/:field', async function (req, res) {
 //   `idTeacher` int NOT NULL,
 //   `previewDocument` varchar(1000) DEFAULT NULL,
 //   `status` varchar(15) NOT NULL,
-router.get('/add', async function (req, res) {
-    res.render('vwCourses/add');
+router.get("/add", async function (req, res) {
+  res.render("vwCourses/add");
 });
 
-router.post('/add', async function (req, res) {
-    const lastModified = moment(req.body.lastModified, "DD/MM/YYYY").format("YYYY-MM-DD");
-    const new_course = {
-        imagePath: req.body.imagePath,
-        videoPath: req.body.videoPath,
-        price: req.body.price,
-        field: req.body.field,
-        title: req.body.title,
-        description: req.body.description,
-        detail: req.body.detail,
-        lastModified: lastModified,
-        idTeacher: req.body.idTeacher,
-        previewDocument: req.body.previewDocument,
-        status: req.body.status
-    };
+router.post("/add", async function (req, res) {
+  const lastModified = moment(req.body.lastModified, "DD/MM/YYYY").format(
+    "YYYY-MM-DD"
+  );
+  const new_course = {
+    imagePath: req.body.imagePath,
+    videoPath: req.body.videoPath,
+    price: req.body.price,
+    field: req.body.field,
+    title: req.body.title,
+    description: req.body.description,
+    detail: req.body.detail,
+    lastModified: lastModified,
+    idTeacher: req.body.idTeacher,
+    previewDocument: req.body.previewDocument,
+    status: req.body.status,
+  };
 
-    await courseModel.add(new_course);
-    res.render('vwCourses/add');
+  await courseModel.add(new_course);
+  res.render("vwCourses/add");
 });
 
-router.get('/edit/:id', async function (req, res) {
-    const id = req.params.id;
-    const course = await courseModel.single(id);
-    if (course === null) {
-        return res.redirect('/admin/courses');
-    }
-    course.lastModified = moment(course.lastModified, "YYYY-MM-DD").format("DD/MM/YYYY");
+router.get("/edit/:id", async function (req, res) {
+  const id = req.params.id;
+  const course = await courseModel.single(id);
+  if (course === null) {
+    return res.redirect("/admin/courses");
+  }
+  course.lastModified = moment(course.lastModified, "YYYY-MM-DD").format(
+    "DD/MM/YYYY"
+  );
 
-    res.render('vwCourses/edit', {
-        course
-    })
+  res.render("vwCourses/edit", {
+    course,
+  });
 });
 
-router.post('/delete/', async function (req, res) {
-    await courseModel.delete(+req.body.id);
-    res.redirect('/admin/courses');
+router.post("/delete/", async function (req, res) {
+  await courseModel.delete(+req.body.id);
+  res.redirect("/admin/courses");
 });
 
-router.post('/patch/', async function (req, res) {
-    await courseModel.patch(req.body);
-    res.redirect('/admin/courses');
+router.post("/patch/", async function (req, res) {
+  await courseModel.patch(req.body);
+  res.redirect("/admin/courses");
 });
 
 //link /admin/courses/isAvailable?idTeacher={{idTeacher}}
-router.get("/isAvailable", async function(req, res) {
-    const idTeacher = req.query.teacher;
-    const teacher = await courseModel.singleByIdTeacher(idTeacher);
-    console.log(teacher);
-    if(teacher === null) {
-        return res.json(false);
-
-    } else {
-        return res.json(true);
-    }
+router.get("/isAvailable", async function (req, res) {
+  const idTeacher = req.query.teacher;
+  const teacher = await courseModel.singleByIdTeacher(idTeacher);
+  console.log(teacher);
+  if (teacher === null) {
+    return res.json(false);
+  } else {
+    return res.json(true);
+  }
 });
-
 
 module.exports = router;
